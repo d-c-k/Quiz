71ca29c6076cb2982df817bd25390ef3ee9e0af1
Daniel Koefoed <dc.koefoed@gmail.com>
2020-10-01 11:27:26 +0200

    linked currentScore to player.score

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", function(e) {
     let player = new Player();
+    let currentScore = document.getElementById("currentScore");
+    currentScore.innerHTML = player.score;
     let btnPlay = document.getElementById("btnPlay");
 
     btnPlay.addEventListener("mousedown", function(e) {
@@ -7,10 +9,7 @@ window.addEventListener("DOMContentLoaded", function(e) {
         console.log(player);
     })
     btnPlay.addEventListener("mouseup", function(e) {
-
-        let regAll = document.getElementById("regAll").style.display = "none"; 
-        let questionsAll = document.getElementById("questionsAll").style.display = "block";
-        
-        console.log("slayer");
+        document.getElementById("regAll").style.display = "none"; 
+        document.getElementById("questionsAll").style.display = "block";
     })
 })
