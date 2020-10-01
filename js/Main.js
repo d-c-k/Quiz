e1c0e16e593c7c7d1e2372e0f6a56604c0e53e8f
Daniel Koefoed <dc.koefoed@gmail.com>
2020-10-01 20:28:32 +0200

    new checkbox-button for answers, started linking question-data to HTML

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", function(e) {
     let player = new Player();
+    let questions = new Questions();
     let currentScore = document.getElementById("currentScore");
     currentScore.innerHTML = player.score;
     let btnPlay = document.getElementById("btnPlay");
@@ -9,7 +10,17 @@ window.addEventListener("DOMContentLoaded", function(e) {
         console.log(player);
     })
     btnPlay.addEventListener("mouseup", function(e) {
+        //questions.fetchQuestion();
+        questions.newQuestion();
         document.getElementById("regAll").style.display = "none"; 
         document.getElementById("questionsAll").style.display = "block";
+    })
+
+    let answers = document.getElementById("answers");
+    
+    answers.addEventListener("click", function(e) {
+        
+        console.log(data)
+        console.log(Questions)
     })
 })
--- js/Questions.js
+++ js/Questions.js
@@ -1,9 +1,50 @@
 class Questions {
-    constructor(nr) {
-        this.nr = nr;
+    constructor(id = 0, questionObj = '') {
+        this.id = id;
+        this.questionObj = questionObj;
+        this.answersObj = [];
+        this.correctAnswersObj = [];
     }
 
     fetchQuestion() {
-        
+        // let num = document.getElementById("list");
+        // let url = "https://quizapi.io/api/v1/questions?apiKey=V2SqyAql6TQH3KSslxE0L7hulaIiaFrZ3BeXbDqn&limit=" + num.value;
+        // fetch(url)
+        // .then((response) => response.json())
+        // .then((data) => {
+        //     for (let i = 0; i <= data.length; i++) {
+        //         this.id = i + 1;
+        //         this.questionObj = data[i].question;
+        //         this.answersObj.push(data[i].answers);
+        //         this.correctAnswersObj.push(data[i].correctAnswers);
+        //     }
+        // });
+    }
+
+    newQuestion() {
+        //--------------
+        let num = document.getElementById("list");
+        let url = "https://quizapi.io/api/v1/questions?apiKey=V2SqyAql6TQH3KSslxE0L7hulaIiaFrZ3BeXbDqn&limit=" + num.value;
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        //--------------
+
+        let questionId = document.getElementById("questionCounter");
+        questionId.innerHTML = "Question " + data[0].index + 1;
+
+        let questionWrite = document.getElementById("question");
+        questionWrite.innerHTML = data[0].question;
+
+        let answerCheck = document.getElementById("c1");
+        answerCheck.innerHTML = data[0].answer[0];
+
+        //---
+        });
+        //---
+    }
+
+    correct() {
+
     }
 }
