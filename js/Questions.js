772d55279c8f98faf2fbbb1743b7de8e0ba32418
Daniel Koefoed <dc.koefoed@gmail.com>
2020-10-04 14:00:00 +0200

    fixed bug with question-counter getting stuck before last question, translated all text to english

--- js/Questions.js
+++ js/Questions.js
@@ -25,18 +25,14 @@ class Questions {
         .then((response) => response.json())
         .then((data) => { 
             for (let i = 0; i < num.value; i++) {
-                //this.id = i + 1;
                 this.questionObj.push(data[i].question);
-                // this.answersObj = Object.values(data[i].answers);
-                // this.correctAnswersObj = Object.values(data[i].correct_answers);
+
                 tmp = Object.values(data[i].answers);
                 this.answersObj.push(tmp);
+
                 tmp2 = Object.values(data[i].correct_answers);
                 this.correctAnswersObj.push(tmp2);
             }
-
-            // this.answersObj = Object.values(data.answers);
-            // this.correctAnswersObj = Object.values(data.correct_answers);
 
             console.log(this.questionObj);
             console.log(this.answersObj);
@@ -57,17 +53,17 @@ class Questions {
         
         await fetch(url);
 
-        for (let i = 1; i <= this.answersObj[this.id].length; i++) {
+        for (let i = 1; i <= this.answersObj[this.id - 1].length; i++) {
             document.getElementById("d" + i).style.display = "block";
         }
 
         let questionId = document.getElementById("questionCounter");
-        questionId.innerHTML = "Question " + this.id;
+        questionId.innerHTML = "Question " + this.id + ":";
 
         let questionWrite = document.getElementById("question");
         questionWrite.innerHTML = this.questionObj[this.id - 1].replace(/\</g,"&lt;");
             
-        for (let i = 0; i < this.answersObj[this.id].length; i++) {
+        for (let i = 0; i < this.answersObj[this.id - 1].length; i++) {
             if (this.answersObj[this.id - 1][i] != undefined) {
                 this.answerLabel[i].innerHTML = this.answersObj[this.id - 1][i].replace(/\</g,"&lt;");
             } else {
